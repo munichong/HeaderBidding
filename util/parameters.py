a2b4fb1e4ae52cd36147209a3963ce069a6e7208
chong.wang <chong.wang@thomsonreuters.com>
2018-03-13 17:00:30 -0400

    Finish NetworkImpressionsClass; Separate parameters definitions

---
+++ util/parameters.py
@@ -0,0 +1,7 @@
+HEADER_BIDDING_KEYS = ('mnetbidprice',
+                       'mnet_abd',
+                       'mnet_fbcpm',
+                       'amznbid',
+                       'fb_bid_price_cents')
+
+FORBES_API_ROOT = 'https://forbesapis.forbes.com/forbesapi/content/all.json/?code=a6016ad7796e2165bba73787d68f3162b29f9bd7'
