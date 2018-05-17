0c0a6d1be3e7df0d6ce11531b132824ad915b360
munichong <munichong@outlook.com>
2018-05-17 10:01:56 -0400

    update the evaluate function and capture logloss update

--- survival_analysis/ParametricSurvivalModels.py
+++ survival_analysis/ParametricSurvivalModels.py
@@ -7,7 +7,7 @@ from survival_analysis.Distributions import WeibullDistribution
 
 class ParametricSurvival:
 
-    def __init__(self, distribution, batch_size, num_epochs, k=1, learning_rate=0.0001):
+    def __init__(self, distribution, batch_size, num_epochs, k=1, learning_rate=0.001):
         self.distribution = distribution
         self.batch_size = batch_size
         self.num_epochs = num_epochs
@@ -21,6 +21,9 @@ class ParametricSurvival:
         feat_vals = tf.tile(tf.expand_dims(predictors, axis=-1), [1, 1, 1])
         feat_x_weights = tf.reduce_sum(tf.multiply(weights, feat_vals), axis=1)
         intercept = tf.Variable(tf.constant(0.1))
+
+        # tf.Assert(tf.reduce_any(tf.is_nan(feat_x_weights)), [tf.is_nan(feat_x_weights)])
+
         return tf.exp(tf.squeeze(feat_x_weights + intercept, [-1]))
 
     def factorization_machines(self):
@@ -42,7 +45,7 @@ class ParametricSurvival:
         time = tf.placeholder(tf.float32, shape=[None], name='time')
         event = tf.placeholder(tf.int32, shape=[None], name='event')
 
-        embeddings = tf.Variable(tf.truncated_normal(shape=(num_features, self.k), mean=0.0, stddev=0.5))
+        embeddings = tf.Variable(tf.truncated_normal(shape=(num_features, self.k), mean=0.0, stddev=0.02))
 
 
         if self.k == 1:
@@ -61,10 +64,13 @@ class ParametricSurvival:
                             self.distribution.left_censoring(time, Lambda),
                             self.distribution.right_censoring(time, Lambda))
 
+        # tf.Assert(tf.reduce_any(tf.is_nan(survival)), [tf.is_nan(survival)])
+
         not_survival = 1 - survival
 
 
         logloss = tf.losses.log_loss(labels=event, predictions=not_survival, weights=1.0)
+        running_loss, loss_update = tf.metrics.mean(logloss)
         loss_mean = tf.reduce_mean(logloss)
         training_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(loss_mean)
 
@@ -72,8 +78,8 @@ class ParametricSurvival:
         not_survival_binary = tf.where(tf.greater_equal(not_survival, 0.5),
                                        tf.ones(tf.shape(not_survival)),
                                        tf.zeros(tf.shape(not_survival)))
-        auc, auc_update = tf.metrics.auc(labels=event, predictions=not_survival_binary, weights=None)
-        acc, acc_update = tf.metrics.accuracy(labels=event, predictions=not_survival_binary, weights=None)
+        running_auc, auc_update = tf.metrics.auc(labels=event, predictions=not_survival_binary, weights=None)
+        running_acc, acc_update = tf.metrics.accuracy(labels=event, predictions=not_survival_binary, weights=None)
 
         # Isolate the variables stored behind the scenes by the metric operation
         running_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="my_metric")
@@ -99,47 +105,42 @@ class ParametricSurvival:
                                                                               time: duration_batch,
                                                                               event: event_batch})
 
-                    auc_batch, acc_batch = sess.run([auc, acc])
-                    # print(loss_batch)
-                    # print(auc_batch)
-                    # print(acc_batch)
-                    # print(Lambda_batch)
+
+                    print(Lambda_batch)
                     print(survival_batch)
-                    print("Epoch %d - Batch %d/%d: loss = %.4f, auc = %.4f, accuracy = %.4f" %
-                          (epoch, num_batch, num_total_batches, loss_batch, auc_batch, acc_batch))
+                    print("Epoch %d - Batch %d/%d: batch loss = %.4f" %
+                          (epoch, num_batch, num_total_batches, loss_batch))
 
 
 
                 # evaluation on training data
-                eval_nodes_batch = [loss_mean, auc_update, acc_update, survival]
-                eval_nodes_all = [auc, acc, survival]
+                eval_nodes_update = [loss_update, auc_update, acc_update]
+                eval_nodes_metric = [running_loss, running_auc, running_acc]
                 print()
                 print("========== Evaluation at Epoch %d ==========" % epoch)
-                loss_train, auc_train, acc_train = self.evaluate(train_data, sess, eval_nodes_batch, eval_nodes_all)
+                loss_train, auc_train, acc_train = self.evaluate(train_data.make_batch(self.batch_size),
+                                                                 sess, eval_nodes_update, eval_nodes_metric)
                 print("*** On Training Set:\tloss = %.6f\tauc = %.4f\taccuracy = %.4f" % (loss_train, auc_train, acc_train))
 
                 # evaluation on validation data
-                print()
-                print("========== Evaluation at Epoch %d ==========" % epoch)
-                loss_val, auc_val, acc_val = self.evaluate(val_data, sess, eval_nodes_batch, eval_nodes_all)
+                loss_val, auc_val, acc_val = self.evaluate(val_data.make_batch(self.batch_size),
+                                                           sess, eval_nodes_update, eval_nodes_metric)
                 print("*** On Validation Set:\tloss = %.6f\tauc = %.4f\taccuracy = %.4f" % (loss_val, auc_val, acc_val))
 
                 # evaluation on test data
-                print()
-                print("========== Evaluation at Epoch %d ==========" % epoch)
-                loss_test, auc_test, acc_test = self.evaluate(test_data, sess, eval_nodes_batch, eval_nodes_all)
+                loss_test, auc_test, acc_test = self.evaluate(test_data.make_batch(self.batch_size),
+                                                              sess, eval_nodes_update, eval_nodes_metric)
                 print("*** On Test Set:\tloss = %.6f\tauc = %.4f\taccuracy = %.4f" % (loss_test, auc_test, acc_test))
 
 
 
-    def evaluate(self, next_batch, sess, eval_nodes_batch, eval_nodes_all):
-        for duration_batch, event_batch, features_batch in sess.run(next_batch):
-            _ = sess.run(eval_nodes_batch,
-                                                        feeddict={'input_vectors:0': features_batch,
-                                                                  'time:0': duration_batch,
-                                                                  'event:0': event_batch})
+    def evaluate(self, next_batch, sess, updates, metrics):
+        for duration_batch, event_batch, features_batch in next_batch:
+            _ = sess.run(updates, feed_dict={'input_vectors:0': features_batch,
+                                             'time:0': duration_batch,
+                                             'event:0': event_batch})
 
-        return sess.run(eval_nodes_all)
+        return sess.run(metrics)
 
 
 if __name__ == "__main__":
@@ -148,7 +149,7 @@ if __name__ == "__main__":
         num_features = int(f.readline())
 
     model = ParametricSurvival(distribution = WeibullDistribution(),
-                    batch_size = 3000,
+                    batch_size = 100000,
                     num_epochs = 30)
     print('Start training...')
     model.run_graph(num_features,
