4d71f09e1bf0780e7e45a382fe0c3bcf24fc7657
munichong <munichong@outlook.com>
2018-05-18 11:58:38 -0400

    minor change

--- survival_analysis/BaselineModels.py
+++ survival_analysis/BaselineModels.py
@@ -18,6 +18,7 @@ class UnivariateLogisticRegression:
         return self.lr.predict(X)
 
     def evaluate(self, X, y_bin_true):
+        print(np.array(X).shape, np.array(y_bin_true).shape)
         y_prob_pred = self.predict_proba(X)
         y_bin_pred = self.predict(X)
         return log_loss(y_bin_true, y_prob_pred), roc_auc_score(y_bin_true, y_bin_pred), accuracy_score(y_bin_true, y_bin_pred)
@@ -29,18 +30,18 @@ if __name__ == '__main__':
         return pickle.load(open(file_path, 'rb'))
 
     def expand_dims(data, axis):
-        return np.expand_dims(d, axis=axis)
+        return np.expand_dims(data, axis=axis)
 
     baseline = UnivariateLogisticRegression()
 
     times_train, events_train = read_data('../Vectors_train.p')[:2]
-    times_train, events_train = expand_dims(times_train, axis=1), events_train
+    times_train = expand_dims(times_train, axis=1)
     times_val, events_val = read_data('../Vectors_val.p')[:2]
-    times_val, events_val = expand_dims(times_val, axis=1), events_val
+    times_val = expand_dims(times_val, axis=1)
     times_test, events_test = read_data('../Vectors_test.p')[:2]
-    times_test, events_test = expand_dims(times_test, axis=1), events_test
+    times_test = expand_dims(times_test, axis=1)
 
     baseline.fit(np.array(times_train), np.array(events_train))
-    print(baseline.evaluate(np.array(times_train), np.array(events_train)))
-    print(baseline.evaluate(np.array(times_val), np.array(events_val)))
-    print(baseline.evaluate(np.array(times_test), np.array(events_test)))
+    print("Training Performance:\tlogloss=%.6f, auc=%.6f, accuracy=%.6f" % baseline.evaluate(np.array(times_train), np.array(events_train)))
+    print("Validation Performance:\tlogloss=%.6f, auc=%.6f, accuracy=%.6f" % baseline.evaluate(np.array(times_val), np.array(events_val)))
+    print("Test Performance:\tlogloss=%.6f, auc=%.6f, accuracy=%.6f" % baseline.evaluate(np.array(times_test), np.array(events_test)))
