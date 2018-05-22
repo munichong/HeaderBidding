2cabe4ac99463e3c1c300e184ee0dcc7dd147c69
Chong Wang <munichong@outlook.com>
2018-05-22 10:49:38 -0400

    print feature selection results as a table

--- survival_analysis/baselines/FeatureSelection.py
+++ survival_analysis/baselines/FeatureSelection.py
@@ -11,31 +11,30 @@ TEST_FILE_PATH = '../../Vectors_test.p'
 def _read_data(file_path):
     return pickle.load(open(file_path, 'rb'))
 
-def chi2_feature_selection(X, y, attr2idx=None):
+def chi2_feature_selection(X, y):
     chi, pval = chi2(X, y)
+    attr2idx = pickle.load(open('../../attr2idx.dict', 'rb'))
+    counter = pickle.load(open('../../counter.dict', 'rb'))
+
     output = []
-    if not attr2idx:
-        print(sorted(enumerate(zip(chi, pval)), key=lambda x:x[1], reverse=True))
-    else:
-        idx2attr = {idx: (attr, feat) for attr, idx_dict in attr2idx.items() for feat, idx in idx_dict.items()}
-        for index, (chi, p) in enumerate(zip(chi, pval)):
-            if index not in idx2attr:
-                print("Index %d is not in the dictionary" % index)
-                field, feat = '<Header Bid>', '<Header Bid>'
-            else:
-                field, feat = idx2attr[index]
+    idx2attr = {idx: (attr, feat) for attr, idx_dict in attr2idx.items() for feat, idx in idx_dict.items()}
+    for index, (chi, p) in enumerate(zip(chi, pval)):
+        if index not in idx2attr:
+            print("Index %d is not in the dictionary" % index)
+            field, feat = '<Header Bid>', '<Header Bid>'
+        else:
+            field, feat = idx2attr[index]
 
-            if not math.isnan(chi):
-                output.append((field, feat, index, chi, p))
+        if not math.isnan(chi):
+            output.append((field, feat, index, counter[field][feat], chi, p))
 
-        print(tabulate(sorted(output, key=lambda x : x[3], reverse=True),
-                        headers=['Field', 'Feature', 'index', 'chi2', 'pval'],
-                        tablefmt='orgtbl'))
+    print(tabulate(sorted(output, key=lambda x : x[-2], reverse=True),
+                    headers=['Field', 'Feature', 'Index', 'Count', 'Chi2', 'P-value'],
+                    tablefmt='orgtbl'))
 
 
 
 if __name__ == '__main__':
     training_data = SurvivalData(*_read_data(TRAIN_FILE_PATH))
-    attr2idx = pickle.load(open('../../attr2idx.dict', 'rb'))
-    chi2_feature_selection(training_data.sparse_features, training_data.events, attr2idx=attr2idx)
+    chi2_feature_selection(training_data.sparse_features, training_data.events)
     chi2_feature_selection(np.expand_dims(training_data.times, -1), training_data.events)
