6f18d16959aecdf3af34ada3ed7a6b2e43655957
Chong Wang <munichong@outlook.com>
2018-05-22 10:26:45 -0400

    print feature selection results as a table

--- survival_analysis/baselines/FeatureSelection.py
+++ survival_analysis/baselines/FeatureSelection.py
@@ -17,16 +17,16 @@ def chi2_feature_selection(X, y, attr2idx=None):
     if not attr2idx:
         print(sorted(enumerate(zip(chi, pval)), key=lambda x:x[1], reverse=True))
     else:
-        idx2attr = {idx: ':'.join([attr, feat]) for attr, idx_dict in attr2idx.items() for feat, idx in idx_dict.items()}
+        idx2attr = {idx: (attr, feat) for attr, idx_dict in attr2idx.items() for feat, idx in idx_dict.items()}
         for index, (chi, p) in sorted(enumerate(zip(chi, pval)), key=lambda x: x[1], reverse=True):
             if index not in idx2attr:
                 print("Index %d is not in the dictionary" % index)
                 output.append(('<Header Bid>', index, chi, p))
                 continue
-            output.append((idx2attr[index], index, chi, p))
+            output.append((*idx2attr[index], index, chi, p))
             # print("%s(%d)\tchi2 = %.4f, pval = %.4f" % (idx2attr[index], index, chi, p))
         print(tabulate(output,
-                        headers=['Field:Feature', 'index', 'chi2', 'pval'],
+                        headers=['Field', 'Feature', 'index', 'chi2', 'pval'],
                         tablefmt='orgtbl'))
 
 
