7ea5a73d761d205f7db07e8117b4e07d5e1837a4
Chong Wang <munichong@outlook.com>
2018-05-24 16:31:09 -0400

    add check_HdWonWithoutHd

---
+++ data_exploration/check_HdWonWithoutHd.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+
+
+HEADER_BIDDING_KEYS = ('mnetbidprice',
+                        'mnet_abd',
+                        'mnet_fbcpm',
+                        'amznbid',
+                        'crt_pb',
+                        'fb_bid_price_cents')
+
+client = MongoClient()
+col = client['Header_Bidding']['NetworkImpressions']
+
+n_total_hdwon = 0
+n_without_hd = 0
+for doc in col.find():
+    n_total_hdwon += 1
+    if any(k in HEADER_BIDDING_KEYS for k, v in doc['CustomTargeting']):
+        continue
+    n_without_hd += 1
+
+print(n_without_hd, n_total_hdwon, n_without_hd / n_total_hdwon)
+
