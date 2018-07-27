93f04cb391d1a5c297e494c2013716ff3e1bb7c1
munichong <munichong@outlook.com>
2018-07-27 15:52:59 -0400

    Update revenue vs floor

--- data_exploration/revenue_vs_floor.py
+++ data_exploration/revenue_vs_floor.py
@@ -8,12 +8,23 @@ col = client['Header_Bidding']['NetworkBackfillImpressions']
 
 x = []
 y = []
+n = 0
 for doc in col.find():
     revenue = doc['EstimatedBackfillRevenue'] *  1000
     floor = doc['SellerReservePrice']
-    if revenue == floor:
+    if revenue <= floor:
         continue
     x.append(floor)
     y.append(revenue)
+    print(floor, revenue)
 
-plt.plot(x, y, 'o', color='black')
+    if n > 10000:
+        break
+    n += 1
+
+plt.plot(x, y, 'o', markersize=1, color='black')
+plt.xlim(0, 2)
+plt.ylim(0, 2)
+plt.xlabel("Reserve Price")
+plt.ylabel("Impression Revenue")
+plt.show()
