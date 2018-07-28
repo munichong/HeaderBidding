dd7c9469e07d5940485dad74671d17115be853d1
munichong <munichong@outlook.com>
2018-07-27 21:36:09 -0400

    Update revenue vs floor

--- data_exploration/revenue_vs_floor.py
+++ data_exploration/revenue_vs_floor.py
@@ -1,6 +1,7 @@
+import numpy as np
 from pymongo import MongoClient
 import matplotlib.pyplot as plt
-
+from pylab import boxplot
 
 client = MongoClient()
 col = client['Header_Bidding']['NetworkBackfillImpressions']
@@ -16,15 +17,40 @@ for doc in col.find():
         continue
     x.append(floor)
     y.append(revenue)
-    print(floor, revenue)
+    # print(floor, revenue)
 
-    if n > 10000:
+    if n > 500000:
         break
     n += 1
 
-plt.plot(x, y, 'o', markersize=1, color='black')
+
+bins = np.array(list(range(0, 21))) / 10
+inds = np.digitize(x, bins)
+print(bins)
+print(inds)
+
+x_bin = []
+y_bin = []
+for group in range(min(inds), max(inds)):
+    # x_bin.append(x[inds == group])
+    y_bin.append(np.array(y)[inds == group])
+
+fig,ax = plt.subplots()
+boxplot(y_bin, 0, '')
+ax.set_xticklabels(bins)
+plt.xlabel("Reserve Price", fontsize=30)
+ax.xaxis.set_tick_params(labelsize=20)
+plt.ylabel("Impression Revenue", fontsize=30)
+ax.yaxis.set_tick_params(labelsize=20)
+plt.show()
+
+
+fig,ax = plt.subplots()
+ax.plot(x, y, '.', markersize=0.3, color='black')
 plt.xlim(0, 2)
-plt.ylim(0, 2)
-plt.xlabel("Reserve Price")
-plt.ylabel("Impression Revenue")
+plt.ylim(0, 4)
+plt.xlabel("Reserve Price", fontsize=30)
+ax.xaxis.set_tick_params(labelsize=20)
+plt.ylabel("Impression Revenue", fontsize=30)
+ax.yaxis.set_tick_params(labelsize=20)
 plt.show()
