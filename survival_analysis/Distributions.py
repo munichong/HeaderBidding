05fb832c33e6561b1df5baa9464cb1bb2f4c57ed
munichong <munichong@outlook.com>
2018-08-11 17:24:30 -0400

    Update Gumbel

--- survival_analysis/Distributions.py
+++ survival_analysis/Distributions.py
@@ -31,23 +31,23 @@ class LogLogisticDistribution:
         return 1 / (scale * time ** self.shape + 1)
 
 
-class GammaDistribution:
-    def __init__(self, shape=1.0):
-        self.shape = shape
-
-    def left_censoring(self, time, scale):
-        return 1- self.right_censoring(time, scale)
-
-    def right_censoring(self, time, scale):
-        return self.shape * tf.igammac(self.shape, scale * time)
+# class GammaDistribution:
+#     def __init__(self, shape=1.0):
+#         self.shape = shape
+#
+#     def left_censoring(self, time, scale):
+#         return 1- self.right_censoring(time, scale)
+#
+#     def right_censoring(self, time, scale):
+#         return self.shape * tf.igammac(self.shape, scale * time)
 
 
 class GumbelDistribution:
-    def __init__(self, shape=1.0):
+    def __init__(self, shape=0.01):
         self.shape = shape  # this param is actually called "location" in Statistics
 
     def left_censoring(self, time, scale):
-        return tf.exp(-1 * tf.exp(time - self.shape) / scale)
+        return tf.exp(-1 * tf.exp((self.shape - time) / scale)) - tf.exp(-1 * tf.exp(self.shape / scale))
 
     def right_censoring(self, time, scale):
-        return 1 - self.left_censoring(time, scale)
+        return 1 - tf.exp(-1 * tf.exp((self.shape - time) / scale))
