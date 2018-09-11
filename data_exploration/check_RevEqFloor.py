fab8e18c740f45bf390c68138745e5faf744a6c2
munichong <munichong@outlook.com>
2018-09-11 11:01:34 -0400

    Add a check on the impressions whose revenue == floor, in which cases reserve price optimization can life revenue

---
+++ data_exploration/check_RevEqFloor.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+
+client = MongoClient()
+col = client['Header_Bidding']['NetworkBackfillImpressions']
+
+tol= 0.0001
+n_total_adxwon = 0
+rev_eq_floor = 0
+for doc in col.find():
+    revenue = doc['EstimatedBackfillRevenue'] *  1000
+    floor = doc['SellerReservePrice']
+    n_total_adxwon += 1
+    if abs(revenue - floor) < tol:
+        rev_eq_floor += 1
+
+print(rev_eq_floor, n_total_adxwon, rev_eq_floor / n_total_adxwon)
