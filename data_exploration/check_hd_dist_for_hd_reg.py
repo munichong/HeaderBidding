8bd32c8ab56e7b173d4c58a511ec068d36e4823c
munichong <munichong@outlook.com>
2018-10-02 07:29:01 -0400

    minor changes

--- data_exploration/check_hd_dist_for_hd_reg.py
+++ data_exploration/check_hd_dist_for_hd_reg.py
@@ -6,19 +6,29 @@ import matplotlib.pyplot as plt
 X_floor_adxwon, Y_maxhd_adxwon = [], []
 X_floor_adxlose, Y_minhd_adxlose = [], []
 n = 0
-for times, events, _, _, min_hds, max_hds, _ in SurvivalData(*pickle.load(open('../TRAIN_SET.p', 'rb'))).make_sparse_batch(100):
-    times, events, min_hds, max_hds = shuffle(times, events, min_hds, max_hds)
-    for t, e, minhd, maxhd in zip(times, events, min_hds, max_hds):
+for times, events, _, _, min_hbs, max_hbs, _ in SurvivalData(*pickle.load(open('../TRAIN_SET.p', 'rb'))).make_sparse_batch(100):
+    times, events, min_hbs, max_hbs = shuffle(times, events, min_hbs, max_hbs)
+    for t, e, minhb, maxhb in zip(times, events, min_hbs, max_hbs):
         if n > 10000:
             break
         print(n)
-        if e == 0 and maxhd and t < maxhd:
+        if (
+                e == 0
+                and maxhb
+                and t < maxhb
+                and (maxhb - t) / t < 0.01
+        ):
             X_floor_adxwon.append(t)
-            Y_maxhd_adxwon.append(maxhd)
+            Y_maxhd_adxwon.append(maxhb)
             n += 1
-        elif e == 1 and minhd and t > minhd:
+        elif (
+                e == 1
+                and minhb
+                and t > minhb
+                and 0.9 < (t - minhb) / t
+        ):
             X_floor_adxlose.append(t)
-            Y_minhd_adxlose.append(minhd)
+            Y_minhd_adxlose.append(minhb)
             n += 1
 
 plt.scatter(X_floor_adxwon, Y_maxhd_adxwon, s=0.3)
