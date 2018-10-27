4de59049b7dca8565862737b6f87d9e2f1cc0ba0
munichong <munichong@outlook.com>
2018-10-27 13:37:33 -0400

    add C-Index

--- survival_analysis/baselines/BaselineUnivariateModels.py
+++ survival_analysis/baselines/BaselineUnivariateModels.py
@@ -2,7 +2,7 @@ import numpy as np, pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss, roc_auc_score, accuracy_score
 from lifelines import KaplanMeierFitter
-
+from survival_analysis.EvaluationMetrics import c_index
 
 class UnivariateLogisticRegression:
 
@@ -26,7 +26,7 @@ class UnivariateLogisticRegression:
         y_bin_pred = self.predict(X)
         # print(y_bin_pred)
         return log_loss(y_bin_true, y_proba_pred, sample_weight=sample_weights), \
-               roc_auc_score(y_bin_true, y_proba_pred, sample_weight=sample_weights), \
+               c_index(y_bin_true, y_proba_pred, np.squeeze(X)), \
                accuracy_score(y_bin_true, y_bin_pred, sample_weight=sample_weights)
 
 
@@ -50,7 +50,7 @@ class KaplanMeier:
         y_proba_pred = self.predict_proba(X)
         y_bin_pred = np.where(y_proba_pred>=0.5, 1.0, 0.0)
         return log_loss(y_bin_true, y_proba_pred, sample_weight=sample_weights), \
-               roc_auc_score(y_bin_true, y_proba_pred, sample_weight=sample_weights), \
+               c_index(y_bin_true, y_proba_pred, np.squeeze(X)), \
                accuracy_score(y_bin_true, y_bin_pred, sample_weight=sample_weights)
 
 
