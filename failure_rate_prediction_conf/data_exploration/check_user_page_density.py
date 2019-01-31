1e2ec55d41149bbde2d6f25ed4e739865f5b243f
munichong <munichong@outlook.com>
2019-01-30 21:58:20 -0500

    minor change

--- failure_rate_prediction_conf/data_exploration/check_user_page_density.py
+++ failure_rate_prediction_conf/data_exploration/check_user_page_density.py
@@ -1,21 +1,24 @@
 import pickle
 from pymongo import MongoClient
 
+USER_MAX_BIN = 200
+PAGE_MAX_BIN = 500
+
 client = MongoClient()
 n_total_imps = client['Header_Bidding']['NetworkImpressions'].find().count() + \
                client['Header_Bidding']['NetworkBackfillImpressions'].find().count()
 
 
-counter = pickle.load(open("../counter.dict", "rb"))  # {Attribute1:Counter<features>, Attribute2:Counter<features>, ...}
+counter = pickle.load(open("../output/counter.dict", "rb"))  # {Attribute1:Counter<features>, Attribute2:Counter<features>, ...}
 
 user_counter = counter['UserId']
 page_counter = counter['NaturalIDs']
 print(user_counter.most_common(100))
 print(page_counter.most_common(100))
 
-user_bin = [0] * 5
+user_bin = [0] * USER_MAX_BIN
 for u, c in user_counter.most_common():
-    if c <= 4:
+    if c <= USER_MAX_BIN - 1:
         user_bin[c - 1] += 1
     else:
         user_bin[-1] += 1
@@ -24,9 +27,9 @@ for i, c in enumerate(user_bin[:-1]):
 print("%d users have >=%d (%f%%) impressions" % (user_bin[-1], len(user_bin), user_bin[-1] / sum(user_bin) * 100))
 print()
 
-page_bin = [0] * 5
+page_bin = [0] * PAGE_MAX_BIN
 for p, c in page_counter.most_common():
-    if c <= 4:
+    if c <= PAGE_MAX_BIN - 1:
         page_bin[c - 1] += 1
     else:
         page_bin[-1] += 1
