638ea6bc90c335a2f1b4103ba4b18c52e51597bf
munichong <munichong@outlook.com>
2019-02-07 07:54:02 -0500

    make shape as a pass-in parameter

--- failure_rate_prediction_conf/Distributions.py
+++ failure_rate_prediction_conf/Distributions.py
@@ -3,33 +3,32 @@ import tensorflow as tf
 
 
 class WeibullDistribution:
-    def __init__(self, shape=0.2, threshold=0.05):
+    def __init__(self, threshold=0.05):
         '''
 
         :param shape: The Weibull distribution with shape = 1 is the exponential distribution
                       A value of shape > 1 indicates that the failure rate increases over time.
         :param threshold:
         '''
-        self.shape = shape
         self.threshold = threshold
 
-    def left_censoring(self, time, scale):
-        return 1 - self.right_censoring(time, scale)
+    def left_censoring(self, time, scale, shape=0.2):
+        return 1 - self.right_censoring(time, scale, shape)
 
-    def right_censoring(self, time, scale):
+    def right_censoring(self, time, scale, shape):
         # with time increases, the return value should decrease
-        return tf.exp(-1 * scale * time ** self.shape)
+        return tf.exp(-1 * scale * time ** shape)
 
 
 class LogLogisticDistribution:
-    def __init__(self, shape=0.7):
-        self.shape = shape
+    def __init__(self):
+        pass
 
-    def left_censoring(self, time, scale):
-        return 1 - self.right_censoring(time, scale)
+    def left_censoring(self, time, scale, shape=0.7):
+        return 1 - self.right_censoring(time, scale, shape)
 
-    def right_censoring(self, time, scale):
-        return 1 / (scale * time ** self.shape + 1)
+    def right_censoring(self, time, scale, shape):
+        return 1 / (scale * time ** shape + 1)
 
 #     def left_censoring(self, time, scale):
 #         return 1 / (time / scale) ** (-1 * self.shape)
@@ -38,50 +37,50 @@ class LogLogisticDistribution:
 #         return 1 - self.left_censoring(time, scale)
 
 class LogisticDistribution:
-    def __init__(self, shape=0.4):
-        self.shape = shape
+    def __init__(self):
+        pass
 
-    def cdf_pointwise(self, time, scale):
-        return 1 / (1 + tf.exp((self.shape - time) / scale))
+    def cdf_pointwise(self, time, scale, shape):
+        return 1 / (1 + tf.exp((shape - time) / scale))
 
-    def left_censoring(self, time, scale):
-        return self.cdf_pointwise(time, scale) - self.cdf_pointwise(0.0, scale)
+    def left_censoring(self, time, scale, shape=0.4):
+        return self.cdf_pointwise(time, scale, shape) - self.cdf_pointwise(0.0, scale, shape)
 
-    def right_censoring(self, time, scale):
-        return 1 - self.cdf_pointwise(time, scale)
+    def right_censoring(self, time, scale, shape):
+        return 1 - self.cdf_pointwise(time, scale, shape)
 
 
 
 class GammaDistribution:
-    def __init__(self, shape=1.0):
-        self.shape = shape
+    def __init__(self):
+        pass
 
-    def left_censoring(self, time, scale):
-        gamma_dist =  tf.distributions.Gamma(concentration=self.shape, rate=scale)
+    def left_censoring(self, time, scale, shape=1.0):
+        gamma_dist =  tf.distributions.Gamma(concentration=shape, rate=scale)
         return gamma_dist.cdf(time)
 
-    def right_censoring(self, time, scale):
-        return 1 - self.left_censoring(time, scale)
+    def right_censoring(self, time, scale, shape=1.0):
+        return 1 - self.left_censoring(time, scale, shape)
 
 
 class GumbelDistribution:
-    def __init__(self, shape=0.001):
-        self.shape = shape  # this param is actually called "location" in Statistics
+    def __init__(self):
+        pass
 
-    def double_exp_part(self, time, scale):
-        return tf.exp(-1 * tf.exp((self.shape - time) / scale))
+    def double_exp_part(self, time, scale, shape): # shape is actually called "location" in Statistics
+        return tf.exp(-1 * tf.exp((shape - time) / scale))
 
-    def left_censoring(self, time, scale):
-        return self.double_exp_part(time, scale) - self.double_exp_part(0.0, scale)
+    def left_censoring(self, time, scale, shape=0.001):
+        return self.double_exp_part(time, scale, shape) - self.double_exp_part(0.0, scale, shape)
 
-    def right_censoring(self, time, scale):
-        return 1 - self.double_exp_part(time, scale)
+    def right_censoring(self, time, scale, shape):
+        return 1 - self.double_exp_part(time, scale, shape)
 
 class ExponentialDistribution:
     def __init__(self):
-        self.shape = None
+        pass
 
-    def left_censoring(self, time, scale):
+    def left_censoring(self, time, scale, shape):
         return 1 - self.right_censoring(time, scale)
 
     def right_censoring(self, time, scale):
